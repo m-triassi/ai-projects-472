4a9063b29918f58fd200c7568525b1a4e4fa2003
Massimo Triassi <kidmax14@hotmail.com>
2021-11-08 23:37:58 -0500

    feat: add recursive raycasting is_end function
    handles n by n matrix ...sorta slow?

--- Project 2/skeleton-tictactoe.py
+++ Project 2/skeleton-tictactoe.py
@@ -1,32 +1,44 @@
 # based on code from https://stackabuse.com/minimax-and-alpha-beta-pruning-in-python
 
 import time
+import numpy as np
+
 
 class Game:
 	MINIMAX = 0
 	ALPHABETA = 1
 	HUMAN = 2
 	AI = 3
-	
-	def __init__(self, recommend = True):
-		self.initialize_game()
+
+	def __init__(self, recommend=True):
+		self.initialize_game(4)
 		self.recommend = recommend
-		
-	def initialize_game(self):
-		self.current_state = [['.','.','.'],
-							  ['.','.','.'],
-							  ['.','.','.']]
+
+	def initialize_game(self, n=3, b=None, s=3):
+		if b is None:
+			b = []
+
+		self.last_move = (-1, -1)
+		self.blocks = b
+		self.n = n
+		self.s = s
+		self.block_count = len(b)
+		self.block_symbol = "👎"
+		self.current_state = np.full((n, n), ".").tolist()
+		for block in b:
+			self.current_state[block[0]][block[1]] = self.block_symbol
+
 		# Player X always plays first
 		self.player_turn = 'X'
 
 	def draw_board(self):
 		print()
-		for y in range(0, 3):
-			for x in range(0, 3):
+		for y in range(0, self.n):
+			for x in range(0, self.n):
 				print(F'{self.current_state[x][y]}', end="")
 			print()
 		print()
-		
+
 	def is_valid(self, px, py):
 		if px < 0 or px > 2 or py < 0 or py > 2:
 			return False
@@ -36,36 +48,172 @@ class Game:
 			return True
 
 	def is_end(self):
-		# Vertical win
-		for i in range(0, 3):
-			if (self.current_state[0][i] != '.' and
-				self.current_state[0][i] == self.current_state[1][i] and
-				self.current_state[1][i] == self.current_state[2][i]):
-				return self.current_state[0][i]
-		# Horizontal win
-		for i in range(0, 3):
-			if (self.current_state[i] == ['X', 'X', 'X']):
-				return 'X'
-			elif (self.current_state[i] == ['O', 'O', 'O']):
-				return 'O'
-		# Main diagonal win
-		if (self.current_state[0][0] != '.' and
-			self.current_state[0][0] == self.current_state[1][1] and
-			self.current_state[0][0] == self.current_state[2][2]):
-			return self.current_state[0][0]
-		# Second diagonal win
-		if (self.current_state[0][2] != '.' and
-			self.current_state[0][2] == self.current_state[1][1] and
-			self.current_state[0][2] == self.current_state[2][0]):
-			return self.current_state[0][2]
-		# Is whole board full?
-		for i in range(0, 3):
-			for j in range(0, 3):
-				# There's an empty field, we continue the game
-				if (self.current_state[i][j] == '.'):
-					return None
-		# It's a tie!
-		return '.'
+		horizontal = self.check_e(0, self.last_move) + self.check_w(0, self.last_move) + 1
+		vertical = self.check_n(0, self.last_move) + self.check_s(0, self.last_move) + 1
+		main_diagonal = self.check_sw(0, self.last_move) + self.check_ne(0, self.last_move) + 1
+		second_diagonal = self.check_se(0, self.last_move) + self.check_nw(0, self.last_move) + 1
+
+		if horizontal >= self.s or vertical >= self.s or main_diagonal >= self.s or second_diagonal >= self.s:
+			return self.current_state[self.last_move[0]][self.last_move[1]]
+
+		if not any("." in x for x in self.current_state):
+			return "."
+
+		return None
+
+
+	def check_n(self, count, move):
+		px, py = move
+		if py - 1 < 0:
+			return count
+
+		## Check its a block
+		if self.current_state[px][py - 1] == self.block_symbol or self.current_state[px][py - 1] == ".":
+			return count
+
+		## check its not same, the return none
+
+		## else it must be same so recurse count + 1
+		if self.current_state[px][py] != self.current_state[px][py - 1]:
+			return count
+		else:
+			if count >= self.s:
+				return count
+			return self.check_n(count + 1, (px, py - 1))
+
+	def check_s(self, count, move):
+		px, py = move
+		if py + 1 >= self.n:
+			return count
+
+		## Check its a block
+		if self.current_state[px][py + 1] == self.block_symbol or self.current_state[px][py + 1] == ".":
+			return count
+
+		## check its not same, the return none
+
+		## else it must be same so recurse count + 1
+		if self.current_state[px][py] != self.current_state[px][py + 1]:
+			return count
+		else:
+			if count >= self.s:
+				return count
+			return self.check_s(count + 1, (px, py + 1))
+
+	def check_w(self, count, move):
+		px, py = move
+		if px - 1 < 0:
+			return count
+
+		## Check its a block
+		if self.current_state[px - 1][py] == self.block_symbol or self.current_state[px - 1][py] == ".":
+			return count
+
+		## check its not same, the return none
+
+		## else it must be same so recurse count + 1
+		if self.current_state[px][py] != self.current_state[px - 1][py]:
+			return count
+		else:
+			if count >= self.s:
+				return count
+			return self.check_w(count + 1, (px - 1, py))
+
+	def check_e(self, count, move):
+		px, py = move
+		if px + 1 >= self.n:
+			return count
+
+		## Check its a block
+		if self.current_state[px + 1][py] == self.block_symbol or self.current_state[px + 1][py] == ".":
+			return count
+
+		## check its not same, the return none
+
+		## else it must be same so recurse count + 1
+		if self.current_state[px][py] != self.current_state[px + 1][py]:
+			return count
+		else:
+			if count >= self.s:
+				return count
+			return self.check_e(count + 1, (px + 1, py))
+
+	def check_ne(self, count, move):
+		px, py = move
+		if px - 1 < 0 or py + 1 >= self.n:
+			return count
+
+		## Check its a block
+		if self.current_state[px - 1][py + 1] == self.block_symbol or self.current_state[px - 1][py + 1] == ".":
+			return count
+
+		## check its not same, the return none
+
+		## else it must be same so recurse count + 1
+		if self.current_state[px][py] != self.current_state[px - 1][py + 1]:
+			return count
+		else:
+			if count >= self.s:
+				return count
+			return self.check_ne(count + 1, (px - 1, py + 1))
+
+	def check_nw(self, count, move):
+		px, py = move
+		if px - 1 < 0 or py - 1 < 0:
+			return count
+
+		## Check its a block
+		if self.current_state[px - 1][py - 1] == self.block_symbol or self.current_state[px - 1][py - 1] == ".":
+			return count
+
+		## check its not same, the return none
+
+		## else it must be same so recurse count + 1
+		if self.current_state[px][py] != self.current_state[px - 1][py - 1]:
+			return count
+		else:
+			if count >= self.s:
+				return count
+			return self.check_nw(count + 1, (px - 1, py - 1))
+
+	def check_se(self, count, move):
+		px, py = move
+		if px + 1 >= self.n or py + 1 >= self.n:
+			return count
+
+		## Check its a block
+		if self.current_state[px + 1][py + 1] == self.block_symbol or self.current_state[px + 1][py + 1] == ".":
+			return count
+
+		## check its not same, the return none
+
+		## else it must be same so recurse count + 1
+		if self.current_state[px][py] != self.current_state[px + 1][py + 1]:
+			return count
+		else:
+			if count >= self.s:
+				return count
+			return self.check_se(count + 1, (px + 1, py + 1))
+
+	def check_sw(self, count, move):
+		px, py = move
+		if px + 1 >= self.n or py - 1 < 0:
+			return count
+
+		## Check its a block
+		if self.current_state[px + 1][py - 1] == self.block_symbol or self.current_state[px + 1][py - 1] == ".":
+			return count
+
+		## check its not same, the return none
+
+		## else it must be same so recurse count + 1
+		if self.current_state[px][py] != self.current_state[px + 1][py - 1]:
+			return count
+		else:
+			if count >= self.s:
+				return count
+			return self.check_sw(count + 1, (px + 1, py - 1))
+
 
 	def check_end(self):
 		self.result = self.is_end()
@@ -86,7 +234,7 @@ class Game:
 			px = int(input('enter the x coordinate: '))
 			py = int(input('enter the y coordinate: '))
 			if self.is_valid(px, py):
-				return (px,py)
+				return (px, py)
 			else:
 				print('The move is not valid! Try again.')
 
@@ -116,8 +264,8 @@ class Game:
 			return (1, x, y)
 		elif result == '.':
 			return (0, x, y)
-		for i in range(0, 3):
-			for j in range(0, 3):
+		for i in range(0, self.n):
+			for j in range(0, self.n):
 				if self.current_state[i][j] == '.':
 					if max:
 						self.current_state[i][j] = 'O'
@@ -155,8 +303,8 @@ class Game:
 			return (1, x, y)
 		elif result == '.':
 			return (0, x, y)
-		for i in range(0, 3):
-			for j in range(0, 3):
+		for i in range(0, self.n):
+			for j in range(0, self.n):
 				if self.current_state[i][j] == '.':
 					if max:
 						self.current_state[i][j] = 'O'
@@ -173,7 +321,7 @@ class Game:
 							x = i
 							y = j
 					self.current_state[i][j] = '.'
-					if max: 
+					if max:
 						if value >= beta:
 							return (value, x, y)
 						if value > alpha:
@@ -185,7 +333,7 @@ class Game:
 							beta = value
 		return (value, x, y)
 
-	def play(self,algo=None,player_x=None,player_o=None):
+	def play(self, algo=None, player_x=None, player_o=None):
 		if algo == None:
 			algo = self.ALPHABETA
 		if player_x == None:
@@ -200,30 +348,32 @@ class Game:
 			if algo == self.MINIMAX:
 				if self.player_turn == 'X':
 					(_, x, y) = self.minimax(max=False)
-				else:
 					(_, x, y) = self.minimax(max=True)
-			else: # algo == self.ALPHABETA
+			else:  # algo == self.ALPHABETA
 				if self.player_turn == 'X':
 					(m, x, y) = self.alphabeta(max=False)
 				else:
 					(m, x, y) = self.alphabeta(max=True)
 			end = time.time()
-			if (self.player_turn == 'X' and player_x == self.HUMAN) or (self.player_turn == 'O' and player_o == self.HUMAN):
-					if self.recommend:
-						print(F'Evaluation time: {round(end - start, 7)}s')
-						print(F'Recommended move: x = {x}, y = {y}')
-					(x,y) = self.input_move()
+			if (self.player_turn == 'X' and player_x == self.HUMAN) or (
+					self.player_turn == 'O' and player_o == self.HUMAN):
+				if self.recommend:
+					print(F'Evaluation time: {round(end - start, 7)}s')
+					print(F'Recommended move: x = {x}, y = {y}')
+				(x, y) = self.input_move()
 			if (self.player_turn == 'X' and player_x == self.AI) or (self.player_turn == 'O' and player_o == self.AI):
-						print(F'Evaluation time: {round(end - start, 7)}s')
-						print(F'Player {self.player_turn} under AI control plays: x = {x}, y = {y}')
+				print(F'Evaluation time: {round(end - start, 7)}s')
+				print(F'Player {self.player_turn} under AI control plays: x = {x}, y = {y}')
+			self.last_move = (x, y)
 			self.current_state[x][y] = self.player_turn
 			self.switch_player()
 
+
 def main():
 	g = Game(recommend=True)
-	g.play(algo=Game.ALPHABETA,player_x=Game.AI,player_o=Game.AI)
-	g.play(algo=Game.MINIMAX,player_x=Game.AI,player_o=Game.HUMAN)
+	g.play(algo=Game.ALPHABETA, player_x=Game.AI, player_o=Game.AI)
+	g.play(algo=Game.MINIMAX, player_x=Game.AI, player_o=Game.HUMAN)
+
 
 if __name__ == "__main__":
 	main()
-
